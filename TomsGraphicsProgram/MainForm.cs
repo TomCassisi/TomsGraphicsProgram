b25bfab3d50f160d418c66357e4adfa2a4cfc2a7
Chris Cameron <chris.cameron@wedoresi.com>
2016-06-11 07:48:40 -0400

    Simple bitmap drawing test

---
+++ TomsGraphicsProgram/MainForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TomsGraphicsProgram
+{
+    public partial class MainForm : Form
+    {
+	    private readonly Bitmap m_Bitmap;
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+        public MainForm()
+        {
+            InitializeComponent();
+
+			m_Bitmap = new Bitmap(ClientSize.Width, ClientSize.Height);
+        }
+
+		/// <summary>
+		/// Destructor.
+		/// </summary>
+	    ~MainForm()
+	    {
+		    m_Bitmap.Dispose();
+	    }
+
+		/// <summary>
+		/// Called to repaint the form.
+		/// </summary>
+		/// <param name="e"></param>
+	    protected override void OnPaint(PaintEventArgs e)
+	    {
+		    base.OnPaint(e);
+
+		    for (int x = 0; x < ClientSize.Width; x++)
+		    {
+			    for (int y = 0; y < ClientSize.Height; y++)
+			    {
+				    int r = (int)Math.Floor((float)x / ClientSize.Width * 255);
+					int g = (int)Math.Floor((float)y / ClientSize.Height * 255);
+				    int b = 0;
+
+					Color color = Color.FromArgb(r, g, b);
+
+				    m_Bitmap.SetPixel(x, y, color);
+			    }
+		    }
+
+			e.Graphics.DrawImage(m_Bitmap, 0, 0);
+	    }
+    }
+}
--- TomsGraphicsProgram/Program.cs
+++ TomsGraphicsProgram/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Windows.Forms;
 
 namespace TomsGraphicsProgram
@@ -13,43 +11,9 @@ namespace TomsGraphicsProgram
         [STAThread]
         static void Main()
         {
-            /*
-            int number = 0;
-            AddNumber(number);
-            MessageBox.Show(number.ToString());
-
-
-            List<string> names = new List<string>();
-            SomeMethod(names);
-            MessageBox.Show(names[0]);
-
-
-            */
-            string something = "some text";
-            Dictionary<string, string> myDict = new Dictionary<string, string>();
-
-            myDict["my key"] = "my value";
-
-            MessageBox.Show(myDict["my key"]);
-
-
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-        }
-
-
-        public static int AddNumber(int number)
-        {
-            number = number + 1;
-            MessageBox.Show(number.ToString());
-
-            return number;
-        }
-
-        public static void SomeMethod(List<string> names)
-        {
-            names.Add("Tom");
+            Application.Run(new MainForm());
         }
     }
 }
