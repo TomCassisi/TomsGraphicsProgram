4de65ee6f92b3ea506986e1ca009a30153131afb
Tom Cassisi <itsmemyselfandtom@googlemail.com>
2016-06-12 19:15:04 +0100

    Adding Pythagoras Method!

---
+++ TomsMathsLib/MathUtils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TomsMathsLib
+{
+    public static class MathUtils
+    {
+        /// <summary>
+        /// Pythagoras Theory
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public static float Pythagoras(float x, float y)
+        {
+            float xSquared = x * x;
+            float ySquared = y * y;
+
+            float z = (float)Math.Sqrt(xSquared + ySquared);
+
+            return z;
+        }
+    }
+}
