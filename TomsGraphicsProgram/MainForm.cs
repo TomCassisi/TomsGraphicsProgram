e9506a95614988936a5a254a515f2e7c504c42f0
Tom Cassisi <itsmemyselfandtom@googlemail.com>
2016-06-12 14:36:42 +0100

    Fixed indentation

--- TomsGraphicsProgram/MainForm.cs
+++ TomsGraphicsProgram/MainForm.cs
@@ -4,51 +4,51 @@ using System.Windows.Forms;
 
 namespace TomsGraphicsProgram
 {
-    public partial class MainForm : Form
-    {
-	    private readonly Bitmap m_Bitmap;
+	public partial class MainForm : Form
+	{
+		private readonly Bitmap m_Bitmap;
 
 		/// <summary>
 		/// Constructor.
 		/// </summary>
-        public MainForm()
-        {
-            InitializeComponent();
+		public MainForm()
+		{
+			InitializeComponent();
 
 			m_Bitmap = new Bitmap(ClientSize.Width, ClientSize.Height);
-        }
+		}
 
 		/// <summary>
 		/// Destructor.
 		/// </summary>
-	    ~MainForm()
-	    {
-		    m_Bitmap.Dispose();
-	    }
+		~MainForm()
+		{
+			m_Bitmap.Dispose();
+		}
 
 		/// <summary>
 		/// Called to repaint the form.
 		/// </summary>
 		/// <param name="e"></param>
-	    protected override void OnPaint(PaintEventArgs e)
-	    {
-		    base.OnPaint(e);
+		protected override void OnPaint(PaintEventArgs e)
+		{
+			base.OnPaint(e);
 
-		    for (int x = 0; x < ClientSize.Width; x++)
-		    {
-			    for (int y = 0; y < ClientSize.Height; y++)
-			    {
-				    int r = (int)Math.Floor((float)x / ClientSize.Width * 255);
+			for (int x = 0; x < ClientSize.Width; x++)
+			{
+				for (int y = 0; y < ClientSize.Height; y++)
+				{
+					int r = (int)Math.Floor((float)x / ClientSize.Width * 255);
 					int g = (int)Math.Floor((float)y / ClientSize.Height * 255);
-				    int b = 0;
+					int b = 0;
 
 					Color color = Color.FromArgb(r, g, b);
 
-				    m_Bitmap.SetPixel(x, y, color);
-			    }
-		    }
+					m_Bitmap.SetPixel(x, y, color);
+				}
+			}
 
 			e.Graphics.DrawImage(m_Bitmap, 0, 0);
-	    }
-    }
+		}
+	}
 }
