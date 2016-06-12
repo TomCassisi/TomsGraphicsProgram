9fb16eddf66e128d7c3496fec6c4e7988f8cac90
Tom Cassisi <itsmemyselfandtom@googlemail.com>
2016-06-12 19:15:52 +0100

    Tests!

---
+++ TomsMathsLibTest/MathUtilsTest.cs
@@ -0,0 +1,78 @@
+﻿using TomsMathsLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace TomsMathsLibTest
+{
+    
+    
+    /// <summary>
+    ///This is a test class for MathUtilsTest and is intended
+    ///to contain all MathUtilsTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class MathUtilsTest
+    {
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for Pythagoras
+        ///</summary>
+        [TestMethod()]
+        public void PythagorasTest()
+        {
+            float x = 3; // TODO: Initialize to an appropriate value
+            float y = 4; // TODO: Initialize to an appropriate value
+            float expected = 5; // TODO: Initialize to an appropriate value
+
+            float actual = MathUtils.Pythagoras(x, y);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ TomsMathsLibTest/Vector3Test.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using TomsMathsLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace TomsMathsLibTest
+{
+    /// <summary>
+    ///This is a test class for Vector3Test and is intended
+    ///to contain all Vector3Test Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class Vector3Test
+    {
+        /// <summary>
+        ///A test for Z
+        ///</summary>
+        [TestMethod()]
+        public void ZTest()
+        {
+            Vector3 target = new Vector3(2, 4, 6); // TODO: Initialize to an appropriate value
+            float actual = 6;
+
+            Assert.AreEqual(actual, target.Z);
+        }
+
+        /// <summary>
+        ///A test for Y
+        ///</summary>
+        [TestMethod()]
+        public void YTest()
+        {
+            Vector3 target = new Vector3(2, 4, 6); // TODO: Initialize to an appropriate value
+            float actual = 4;
+
+            Assert.AreEqual(actual, target.Y);
+        }
+
+        /// <summary>
+        ///A test for X
+        ///</summary>
+        [TestMethod()]
+        public void XTest()
+        {
+            Vector3 target = new Vector3(2, 4, 6); // TODO: Initialize to an appropriate value
+            float actual = 2;
+
+            Assert.AreEqual(actual, target.X);
+        }
+
+        /// <summary>
+        ///A test for Vector3 Constructor
+        ///</summary>
+        [TestMethod()]
+        public void Vector3ConstructorTest()
+        {
+            float x = 2; // TODO: Initialize to an appropriate value
+            float y = 4; // TODO: Initialize to an appropriate value
+            float z = 6; // TODO: Initialize to an appropriate value
+
+            Vector3 target = new Vector3(x, y, z);
+
+            Assert.AreEqual(x, target.X);
+            Assert.AreEqual(y, target.Y);
+            Assert.AreEqual(z, target.Z);
+        }
+
+        /// <summary>
+        ///A test for Magnitude
+        ///</summary>
+        [TestMethod()]
+        public void MagnitudeTest()
+        {
+            Vector3 target = new Vector3(3, 4, 5); // TODO: Initialize to an appropriate value
+            float actual;
+            float expected = (float)Math.Sqrt(50);
+
+            actual = target.Magnitude;
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for GetNormalized
+        ///</summary>
+        [TestMethod()]
+        public void GetNormalizedTest()
+        {
+            Vector3 target = new Vector3(3, 4, 5);
+
+            float expected = 1;
+            float actual = target.GetNormalized().Magnitude;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for DotProduct
+        ///</summary>
+        [TestMethod()]
+        public void DotProductTest()
+        {
+            Vector3 target = new Vector3(1, 0, 0);
+            Vector3 other = new Vector3(-1, 0, 0);
+
+            float expected = 0;
+            float actual;
+            actual = target.DotProduct(other);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
