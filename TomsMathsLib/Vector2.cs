586efe852d0fef8412f368e817962866d0f5a123
Tom Cassisi <itsmemyselfandtom@googlemail.com>
2016-06-14 18:01:10 +0100

    Adding Magnitude/Normalizing/DotProduct for Vector2

--- TomsMathsLib/Vector2.cs
+++ TomsMathsLib/Vector2.cs
@@ -22,5 +22,50 @@ namespace TomsMathsLib
             m_Y = y;
         }
 
+        /// <summary>
+        /// Distance from origin to Vector
+        /// </summary>
+        public float Magnitude
+        {
+            get
+            {
+                float magnitude = MathUtils.Pythagoras(m_X, m_Y);
+
+                return magnitude;
+
+            }
+        }
+
+        /// <summary>
+        /// Normalizing magnitude to 1
+        /// </summary>
+        /// <returns></returns>
+        public Vector2 GetNormalized()
+        {
+            float magnitude = Magnitude;
+
+            float x = X / magnitude;
+            float y = Y / magnitude;
+
+            return  new Vector2(x, y);
+
+        }
+
+        /// <summary>
+        /// Calculates the dot product of this vector with another vector.
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public float DotProduct(Vector2 other)
+        {
+            Vector2 thisNormalized = GetNormalized();
+            Vector2 otherNormalized = other.GetNormalized();
+
+            float axbx = thisNormalized.X * otherNormalized.X;
+            float ayby = thisNormalized.Y * otherNormalized.Y;
+
+            return axbx + ayby;
+        }
+
     }
 }
