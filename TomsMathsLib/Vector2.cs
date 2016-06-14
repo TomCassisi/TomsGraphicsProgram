805023636a438b0303f78dbb6c7fc1f2525990ae
Tom Cassisi <itsmemyselfandtom@googlemail.com>
2016-06-14 18:00:37 +0100

    Setting up Constructor for Vector2

--- TomsMathsLib/Vector2.cs
+++ TomsMathsLib/Vector2.cs
@@ -4,6 +4,23 @@ namespace TomsMathsLib
 {
     public struct Vector2
     {
+        private readonly float m_X;
+        private readonly float m_Y;
+
+        // Properties for the Vector 2 postions
+        public float X { get { return m_X; } }
+        public float Y { get { return m_Y; } }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public Vector2(float x, float y)
+        {
+            m_X = x;
+            m_Y = y;
+        }
 
     }
 }
