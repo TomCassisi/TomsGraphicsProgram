fce7b72c772a9f25a1457ef9f3a2ad7d3078cc35
Tom Cassisi <itsmemyselfandtom@googlemail.com>
2016-06-14 17:00:36 +0100

    Implementing adding subtracting and negative vectors

--- TomsMathsLib/Vector3.cs
+++ TomsMathsLib/Vector3.cs
@@ -80,7 +80,7 @@ namespace TomsMathsLib
 		/// <returns></returns>
 		public static Vector3 operator +(Vector3 v1, Vector3 v2)
 		{
-			throw new NotImplementedException();
+            return new Vector3((v1.X + v2.X), (v1.Y + v2.Y), (v1.Z + v2.Z));
 		}
 
 		/// <summary>
@@ -91,7 +91,7 @@ namespace TomsMathsLib
 		/// <returns></returns>
 		public static Vector3 operator -(Vector3 v1, Vector3 v2)
 		{
-			throw new NotImplementedException();
+            return new Vector3((v1.X - v2.X), (v1.Y - v2.Y), (v1.Z - v2.Z));
 		}
 
 		/// <summary>
@@ -101,7 +101,7 @@ namespace TomsMathsLib
 		/// <returns></returns>
 		public static Vector3 operator -(Vector3 v1)
 		{
-			throw new NotImplementedException();
+            return new Vector3(0, 0 , 0) -v1;
 		}
 	}
 }
