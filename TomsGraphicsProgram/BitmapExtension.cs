e4dcfb667a49bb7ea09c9a2f1f30832923f825cb
Tom Cassisi <itsmemyselfandtom@googlemail.com>
2016-06-15 18:49:15 +0100

    Created BitmapExtensions so we can SetPixels and Drawlines

---
+++ TomsGraphicsProgram/BitmapExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using TomsMathsLib;
+
+namespace TomsGraphicsProgram
+{
+    public static class BitmapExtension
+    {
+        public static void SetPixel(this Bitmap extends, Vector2 postion , Color color)
+        {
+            int x = (int)postion.X;
+            int y = (int)postion.Y;
+
+            extends.SetPixel(x, y, color);
+        }
+
+        public static void DrawLine(this Bitmap myBitmap, Vector2 start, Vector2 end, Color color)
+        {
+            Vector2 midPoint = new Vector2();
+
+            midPoint =(start + end) / 2;
+
+            DrawLine(myBitmap, start, midPoint, color);
+            DrawLine(myBitmap, midPoint, end, color);
+        }
+    }
+}
